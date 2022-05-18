f9566690faa2d4ecc3c9bee264187b40739b7c3b
Aditya Khurana <adi831997@gmail.com>
2022-05-18 18:09:25 +0530

    Create Election_Contract.sol

---
+++ Election_Contract.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.2;
+
+//SPDX-License-Identifier: MIT
+
+contract Election {
+    
+    // Model a Candidate
+
+    struct  Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    
+    address public owner;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;   
+
+    function addCandidate(string memory _name) public {
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidatesCount++;
+    }
+
+    function Vote(uint _candidateId) public {
+    
+    // require that they haven't voted before
+    require(voters[msg.sender] == false, "User has already casted vote !!");
+
+    // require a valid candidate
+    require(candidates[_candidateId].id == _candidateId);
+
+    // record that voter has voted
+    voters[msg.sender] = true;
+
+    // update candidate vote Count
+    candidates[_candidateId].voteCount++;
+
+
+    }
+}
