c52d0becefff4904236c81335a03cd8a4be83655
Aditya Khurana <adi831997@gmail.com>
2022-05-29 23:19:06 +0530

    Update ASSIGNMENT2.sol

--- ASSIGNMENT2.sol
+++ ASSIGNMENT2.sol
@@ -1,3 +1,18 @@
 1. NFT staking contract which accrues rewards in ERC 20
 2. Creating & deploying a ERC 721 token
 
+// Creating and deploying ERC- 721 NFT Code
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+import https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol,
+import https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol,
+contract newNFT is IERC721, NFTokenMetadata, Ownable {
+  constructor() {
+    nftName = MyNFT;
+    nftSymbol = MYN;
+  }
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+  super._mint(_to, _tokenId);
+  super._setTokenUri(_tokenId, _uri);
+  }
+}
