ad0dc3718654af06a74251406b6700e59d6901d5
Aditya Khurana <adi831997@gmail.com>
2022-05-29 22:54:07 +0530

    Create Counter.sol

---
+++ SmartContactProgrammer/Counter.sol
@@ -0,0 +1,11 @@
+//SPDX-LicenseIdentifier: MIT
+pragma solidity ^0.8.7;
+contract Counter {
+  uint public count;
+  function inc() external {
+    count += 1;
+  }
+  function dec() external {
+    count -= 1;
+  }
+}
